3f39b52a99ea60f681c75ea082c3fb54d5c9f829
Apoorva Sharma <apoorva.sharma@comprotechnologies.com>
2023-07-10 14:19:40 +0530

    minor changes

--- main.py
+++ main.py
@@ -42,29 +42,31 @@ agent = initialize_agent(
 st.title('Generate description and identify objects in an image')
 
 # upload file
-file = st.file_uploader("Please upload an image",
-                        type=["jpeg", "jpg", "png"])
+# file = st.file_uploader("Please upload an image",
+#                         type=["jpeg", "jpg", "png"])
 user_question_1 = "generate a caption for this image?"
 user_question_2 = "Please tell me what are the items present in the image."
 
-if file:
-    # display image
-    st.image(file, use_column_width=True)
+# if file:
+#     # display image
+#     st.image(file, use_column_width=True)
 
-    # Save the file to a temporary directory
-    temp_dir = tempfile.TemporaryDirectory()
-    file_path = os.path.join(temp_dir.name, file.name)
-    with open(file_path, "wb") as f:
-        f.write(file.getbuffer())
+#     # Save the file to a temporary directory
+#     temp_dir = tempfile.TemporaryDirectory()
+#     file_path = os.path.join(temp_dir.name, file.name)
+#     with open(file_path, "wb") as f:
+#         f.write(file.getbuffer())
 
-    # write agent response
-    with st.spinner(text="In progress..."):
-        response1 = agent.run(
-            f'{user_question_1}, this is the image path: {file_path}')
-        st.write("The description of the image is: " + response1)
-        response2 = agent.run(
-            f'{user_question_2}, this is the image path: {file_path}')
-        st.write(response2)
+file_path = 'C:/Users/Compro/Downloads/hqdefault.jpg'
 
-        # Clean up the temporary directory
-        temp_dir.cleanup()
+# write agent response
+with st.spinner(text="In progress..."):
+    response1 = agent.run(
+        f'{user_question_1}, this is the image path: {file_path}')
+    st.write("The description of the image is: " + response1)
+    response2 = agent.run(
+        f'{user_question_2}, this is the image path: {file_path}')
+    st.write(response2)
+
+    # Clean up the temporary directory
+    temp_dir.cleanup()
