1cf5672a9a0a29f2868632bba77498deca0c08d5
Apoorva Sharma <apoorva.sharma@comprotechnologies.com>
2023-07-11 11:36:46 +0530

    minor changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from langchain.agents import initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-
+import functions
 
 
 from tools import ImageCaptionTool, ObjectDetectionTool
@@ -42,30 +42,39 @@ agent = initialize_agent(
 st.title('Generate description and identify objects in an image')
 
 # upload file
-# file = st.file_uploader("Please upload an image",
-#                         type=["jpeg", "jpg", "png"])
+file = st.file_uploader("Please upload an image",
+                        type=["jpeg", "jpg", "png"])
 user_question_1 = "generate a caption for this image?"
 user_question_2 = "Please tell me what are the items present in the image."
 
-# if file:
-#     # display image
-file_path = './hqdefault.jpg'
-st.image(file_path, use_column_width=True)
 
-#     # Save the file to a temporary directory
-#     temp_dir = tempfile.TemporaryDirectory()
-#     file_path = os.path.join(temp_dir.name, file.name)
-#     with open(file_path, "wb") as f:
-#         f.write(file.getbuffer())
+# file_path = './hqdefault.jpg'
+# st.image(file_path, use_column_width=True)
+
+if file:
+    # display image
+
+    st.image(file_path, use_column_width=True)
+    # Save the file to a temporary directory
+    temp_dir = tempfile.TemporaryDirectory()
+    file_path = os.path.join(temp_dir.name, file.name)
+    with open(file_path, "wb") as f:
+        f.write(file.getbuffer())
 
     # write agent response
-with st.spinner(text="In progress..."):
-    response1 = agent.run(
-        f'{user_question_1}, this is the image path: {file_path}')
-    st.write("The description of the image is: " + response1)
-    # response2 = agent.run(
-    #     f'{user_question_2}, this is the image path: {file_path}')
-    # st.write(response2)
+    with st.spinner(text="In progress..."):
+        # response1 = agent.run(
+        #     f'{user_question_1}, this is the image path: {file_path}')
 
-    # # Clean up the temporary directory
-    # temp_dir.cleanup()
+        response1 = functions.get_image_caption(file_path)
+        print(response1)
+        st.write("The description of the image is: " + response1)
+        # response2 = agent.run(
+        #     f'{user_question_2}, this is the image path: {file_path}')
+        # st.write(response2)
+        response2 = functions.detect_objects(file_path)
+        print(response2)
+        st.write(response2)
+
+        # Clean up the temporary directory
+        temp_dir.cleanup()
